ae8be3486bb2a76bafc7fc6e924889c799a5f2be
Alexandr <stitel23@mail.ru>
2025-08-21 01:00:11 +0300

    add JS init

---
+++ index.js
@@ -0,0 +1,30 @@
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(getRandomNumber(5, 10));
+
+function getRandomExponent(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(getRandomExponent(2,4));
+
+let Number = function(getRandomNumber);
+
+console.log(Number);
+
+let Exponent = function(getRandomExponent);
+
+function square(Number){
+    return Number * 2
+}
+
+console.log(square(Number));
+
+function NumberInExponent(Number, Exponent) {
+    return 
+}
+
+console.log(NumberInExponent(Number, Exponent))
+
